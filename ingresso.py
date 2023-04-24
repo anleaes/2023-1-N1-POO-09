2ab8d6dc8c5d379a8b14dab9600a9e19aa6757a2
NFTHiKe <97326896+NFTHiKe@users.noreply.github.com>
2023-04-24 19:15:50 -0300

    Ingresso.py
    Criação da classe ingresso em Python

---
+++ ingresso.py
@@ -0,0 +1,10 @@
+class Ingresso:
+    
+    def __init__ (self, idIngresso, horaSessao, filme, cinema, poltrona, valorIngresso, cliente):
+        self._idIngresso = idIngresso
+        self._horaSessao = horaSessao
+        self._filme = filme
+        self._cinema = cinema
+        self._poltrona = poltrona
+        self._valorIngresso = valorIngresso
+        self._cliente = cliente
